1ec694edcf766c62207316b5d0f17328bd40c6e2
AlvaroCeballos <a.ceballos.ivddb@gmail.com>
2024-09-22 22:44:07 -0600

    Prueba lectura xml

---
+++ lecturaXMLprueba.py
@@ -0,0 +1,89 @@
+from xml.dom import minidom
+
+class nodoActualProductoXML:
+    def __init__(self, nombreProductoActual, procesoElabAct):
+        self.nombreProductoActual = nombreProductoActual
+        self.procesoElabAct = procesoElabAct
+        self.siguienteProductoListaA = None
+
+class listaActualProductosXML:
+    def __init__(self):
+        self.primerProductoListaA = None
+
+    def insertarProductoActualListaE(self, nombreProductoActual, procesoElabAct):
+        productoNuevoActualLista = nodoActualProductoXML(nombreProductoActual, procesoElabAct)
+        if not self.primerProductoListaA:
+            self.primerProductoListaA = productoNuevoActualLista
+        else:
+            productoActLista = self.primerProductoListaA
+            while productoActLista.siguienteProductoListaA:
+                productoActLista = productoActLista.siguienteProductoListaA
+            productoActLista.siguienteProductoListaA = productoNuevoActualLista
+
+class nodoMaquinaActualLista:
+    def __init__(self, nomActualMaquina, totalLineasPactual, totalActualNodo, tiempoEnsamblajeA, listaActualProductosMaquina):
+        self.nomActualMaquina = nomActualMaquina
+        self.totalLineasPactual = totalLineasPactual
+        self.totalActualNodo = totalActualNodo
+        self.tiempoEnsamblajeA = tiempoEnsamblajeA
+        self.listaActualProductosMaquina = listaActualProductosMaquina
+        self.sigMaquinaLista = None
+
+class listaMaquinasXML:
+    def __init__(self):
+        self.primerMlistaAct = None
+
+    def agregarMaquinaActualNodo(self, nomActualMaquina, totalLineasPactual, totalActualNodo, tiempoEnsamblajeA, listaActualProductosMaquina):
+        maquinaNuevaActual = nodoMaquinaActualLista(nomActualMaquina, totalLineasPactual, totalActualNodo, tiempoEnsamblajeA, listaActualProductosMaquina)
+        if not self.primerMlistaAct:
+            self.primerMlistaAct = maquinaNuevaActual
+        else:
+            maquinaActualLista = self.primerMlistaAct
+            while maquinaActualLista.sigMaquinaLista:
+                maquinaActualLista = maquinaActualLista.sigMaquinaLista
+            maquinaActualLista.sigMaquinaLista = maquinaNuevaActual
+
+def lecturaXMLActual(pathActualXML):
+    try:
+        parseoActualXML = minidom.parse(pathActualXML)
+        raizActualXML = parseoActualXML.documentElement
+
+        maquinasLecturaActual = raizActualXML.getElementsByTagName('Maquina')
+        listaActualMaquinasLectura = listaMaquinasXML()
+
+        for maquinaActualLexturaXML in maquinasLecturaActual:
+            nomActualMaquina = maquinaActualLexturaXML.getElementsByTagName('NombreMaquina')[0].firstChild.nodeValue.strip()
+            totalLineasPactual = int(maquinaActualLexturaXML.getElementsByTagName('CantidadLineasProduccion')[0].firstChild.nodeValue.strip())
+            totalActualNodo = int(maquinaActualLexturaXML.getElementsByTagName('CantidadComponentes')[0].firstChild.nodeValue.strip())
+            tiempoEnsamblajeA = int(maquinaActualLexturaXML.getElementsByTagName('TiempoEnsamblaje')[0].firstChild.nodeValue.strip())
+
+            productosActualesMaquinaLectura = maquinaActualLexturaXML.getElementsByTagName('Producto')
+            listaActualProductosMaquina = listaActualProductosXML()
+
+            for productoActualLect in productosActualesMaquinaLectura:
+                nombreProductoActual = productoActualLect.getElementsByTagName('nombre')[0].firstChild.nodeValue.strip()
+                procesoElabAct = productoActualLect.getElementsByTagName('elaboracion')[0].firstChild.nodeValue.strip()
+                listaActualProductosMaquina.insertarProductoActualListaE(nombreProductoActual, procesoElabAct)
+
+            listaActualMaquinasLectura.agregarMaquinaActualNodo(nomActualMaquina, totalLineasPactual, totalActualNodo, tiempoEnsamblajeA, listaActualProductosMaquina)
+
+        maquinaActualLista = listaActualMaquinasLectura.primerMlistaAct
+        while maquinaActualLista:
+            print(f"Nombre maquina actual: {maquinaActualLista.nomActualMaquina}")
+            print(f"Total actual de lineas de produccion: {maquinaActualLista.totalLineasPactual}")
+            print(f"Componentes actuales: {maquinaActualLista.totalActualNodo}")
+            print(f"Tiempo total: {maquinaActualLista.tiempoEnsamblajeA} minutos")
+            print("Lista productos maquina actual:")
+            productoActLista = maquinaActualLista.listaActualProductosMaquina.primerProductoListaA
+            while productoActLista:
+                print(f"Nombre actual de producto: {productoActLista.nombreProductoActual}")
+                print(" Elaboracion proceso:")
+                for paso in productoActLista.procesoElabAct.split():
+                    print(f"  - {paso}")
+                productoActLista = productoActLista.siguienteProductoListaA
+            maquinaActualLista = maquinaActualLista.sigMaquinaLista
+
+    except Exception as errorActualLectura:
+        print(f"Error de lectura XML: {errorActualLectura}")
+
+lecturaXMLActual('prueba.xml')
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import os
 app = Flask(__name__)
 app.secret_key = 'mi_clave_secreta_super_segura_123'
 
-Lcarro = []
 
 @app.route('/')
 def index():
@@ -16,44 +15,11 @@ def index():
 
 
 
-@app.route('/submitL', methods=['POST']) 
-def login():
-    usuario = request.form['username']
-    password = request.form['password']
 
-    if usuario == 'admin' and password == '2233AD':
-        print(usuario)
-        print(password)
-        return redirect(url_for('agregar'))
-        
-    else:
-        flash('Credenciales incorrectas. Intenta de nuevo.')
-        return redirect(url_for('index'))
-
-
-@app.route('/Agregar')
-def agregar():
-    return render_template('form.html')
-
-@app.route('/submit', methods=['POST']) 
-def submit():
-    idTipoAuto = request.form['idTipoAuto']
-    Marca = request.form['Marca']
-    Modelo = request.form['Modelo']
-    Descripcion = request.form['Descripcion']
-    Precio_unitario = request.form['Precio_unitario']
-    Cantidad = request.form['Cantidad']
-    Imagen = request.form['Imagen']
-
-    # carroCreado = Carro(idTipoAuto, Marca, Modelo, Descripcion, Precio_unitario, Cantidad, Imagen)
-    # Lcarro.append(carroCreado)
-    # print(carroCreado)
-
-    return redirect(url_for('agregar'))
 
 @app.route('/ayuda')
 def listar():
-    return render_template('index.html', listado=Lcarro)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     slc = 0
@@ -69,3 +35,4 @@ def CargarArchivo(rutaArchivo):
     else:
         print('El archivo no existe')
         return False
+    
